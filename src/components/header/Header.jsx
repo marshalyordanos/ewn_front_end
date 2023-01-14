d44e695339420869972f152e50188d0d63e7f024
marshalyordanos <marshalyordanosseifu@gmail.com>
2023-01-14 06:17:56 -0500

    some change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import logo from "../../assets/images/logo.svg";
 import "./header.css";
 import { User } from "./User";
@@ -8,9 +8,15 @@ import { useSelector } from "react-redux";
 import { user } from "../../redux/authReducer";
 
 export const Header = () => {
+  const [scroll, setScroll] = useState(false);
   window.addEventListener("scroll", function () {
     const header = this.document.querySelector(".header");
     header.classList.toggle("active", this.window.scrollY > 100);
+    if (this.window.scrollY > 100) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
   });
   const users = useSelector(user);
   return (
@@ -18,7 +24,11 @@ export const Header = () => {
       <header className="header">
         <div className="scontainer flex">
           <div className="logo">
-            <img src={logo} alt="logo" width="100px" />
+            {scroll ? (
+              <img src={"/logo1.png"} alt="logo" width="100px" />
+            ) : (
+              <img src={"/logo2.png"} alt="logo" width="100px" />
+            )}
           </div>
           <nav>
             <ul>
